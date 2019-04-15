8b0d6be1ed4a11ca5637d0c9d4ddefa1493c8d67
Ilyos <ilyosonline@gmail.com>
2019-04-15 17:50:12 +0500

    1

--- pulpul/js/main.js
+++ pulpul/js/main.js
@@ -1,5 +1,14 @@
+$(".download-btn").hide();
+var checkSlide = function() {
+    if ($('.swiper-home').hasClass("swiper-slide-active") || $('.swiper-contact').hasClass("swiper-slide-active")) {
+        $(".download-btn").hide();
+    } else {
+        $(".download-btn").show();
+    }
+};
+
 var swiperAnimation = new SwiperAnimation();
-var mySwiper = new Swiper('.swiper-vertical-pagination', {
+mySwiper = new Swiper('.swiper-vertical-pagination', {
     pagination: ".swiper-pagination-white",
     direction: "vertical",
     slidesPerView: 1,
@@ -18,11 +27,18 @@ var mySwiper = new Swiper('.swiper-vertical-pagination', {
     on: {
       init: function () {
         swiperAnimation.init(this).animate();
+        checkSlide();
       },
-      slideChange: function () {
+      transitionStart: function () {
         swiperAnimation.init(this).animate();
+        checkSlide();
       }
     }
 })
 
-
+$(function(){
+  $('.download-btn').click(function(e){
+    e.preventDefault()
+     mySwiper.slideTo(9,1000,false );
+  })
+})

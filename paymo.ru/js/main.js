f7f2015fa842df4e4bb831d137d97bbc5190db15
Ilyos <ilyosonline@gmail.com>
2019-08-07 16:18:01 +0500

    1

--- paymo.ru/js/main.js
+++ paymo.ru/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(window).on("load resize",function(e){
 
 
     var window_width = $(window).width();
@@ -127,24 +127,6 @@ $(document).ready(function() {
         });
     });
 
-    function checkValue(element) {
-  // check if the input has any value (if we've typed into it)
-  if ($(element).val())
-    $(element).addClass('has-value');
-  else
-    $(element).removeClass('has-value');
-}
 
-$(document).ready(function() {
-  // Run on page load
-  $('.form-control').each(function() {
-    checkValue(this);
-  })
-  // Run on input exit
-  $('.form-control').blur(function() {
-    checkValue(this);
-  });
-
-});
 
 });

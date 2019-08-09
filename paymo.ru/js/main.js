28415f97d51fa671232577056d0aff91ea179c1e
Ilyos <ilyosonline@gmail.com>
2019-08-09 15:37:35 +0500

    1

--- paymo.ru/js/main.js
+++ paymo.ru/js/main.js
@@ -130,6 +130,105 @@ $(document).ready(function() {
         });
     });
 
+    var $result1 = $("#tarif1 .sum span");
+    var $percent1 = $("#tarif1 .percent");
+    var $result2 = $("#tarif2 .sum span");
+    var $percent2 = $("#tarif2 .percent");
+    $("#tarif1_range").ionRangeSlider({
+        type: "single",
+        grid: true,
+        values: ["0", "1 000 000", "2 500 000", "5 000 000", "от 5 000 000"],
+        from: 1,
+        to: 5,
+        onChange: function(data) {
+            $result1.text(data.from_value);
+            if (data.from_value === "1 000 000") {
+                $percent1.text("3,5 %");
+                $("#tarif1 .percent").addClass('lg-text').removeClass('sm-text');
+            } else if (data.from_value === "2 500 000") {
+                $percent1.text("3 %");
+                $("#tarif1 .percent").addClass('lg-text').removeClass('sm-text');
+            } else if (data.from_value === "5 000 000") {
+                $percent1.text("2,5 %");
+                $("#tarif1 .percent").addClass('lg-text').removeClass('sm-text');
+            } else if (data.from_value === "от 5 000 000") {
+                $percent1.text("Индивидуальная");
+                $("#tarif1 .percent").addClass('sm-text').removeClass('lg-text');
+            }
+        }
+    });
+
+
+
+    $("#tarif2_range").ionRangeSlider({
+        type: "single",
+        grid: true,
+        values: ["0", "1 000 000", "10 000 000", "30 000 000", "50 000 000", "от 50 000 000"],
+        from: 1,
+        to: 5,
+        onChange: function(data) {
+            $result2.text(data.from_value);
+            if (data.from_value === "1 000 000") {
+                $percent2.text("1,5 %");
+                $("#tarif2 .percent").addClass('lg-text').removeClass('sm-text');
+            } else if (data.from_value === "10 000 000") {
+                $percent2.text("1,3 %");
+                $("#tarif2 .percent").addClass('lg-text').removeClass('sm-text');
+            } else if (data.from_value === "30 000 000") {
+                $percent2.text("1 %");
+                $("#tarif2 .percent").addClass('lg-text').removeClass('sm-text');
+            } else if (data.from_value === "50 000 000") {
+                $percent2.text("0,8 %");
+                $("#tarif2 .percent").addClass('sm-text').removeClass('lg-text');
+            } else if (data.from_value === "от 50 000 000") {
+                $percent2.text("Индивидуальная");
+                $("#tarif2 .percent").addClass('sm-text').removeClass('lg-text');
+            } else if (data.from_value === "0") {
+                $percent2.text("1,5 %");
+                $("#tarif2 .percent").addClass('sm-text').removeClass('sm-text');
+            }
+        }
+    });
+
+
+    var swiper = new Swiper('.slider-clients', {
+        allowTouchMove: false,
+        slidesPerView: 'auto',
+        grabCursor: true,
+        spaceBetween: 0,
+        loop: true,
+        freeMode: true,
+        speed: 2000,
+        autoplay: {
+            delay: 0,
+        },
+        keyboardControl: false,
+        mousewheel: {
+            invert: false,
+            forceToAxis: false,
+        },
+        keyboard: {
+            enabled: false
+        },
+    });
+
+    function checkValue(element) {
+        // check if the input has any value (if we've typed into it)
+        if ($(element).val())
+            $(element).addClass('has-value');
+        else
+            $(element).removeClass('has-value');
+    }
+
+
+    // Run on page load
+    $('.form-control').each(function() {
+        checkValue(this);
+    })
+    // Run on input exit
+    $('.form-control').blur(function() {
+        checkValue(this);
+    });
 
 
 });

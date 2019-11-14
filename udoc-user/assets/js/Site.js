a1f43b20db3bee0352af9d33348708b1923d744a
Ilyos <ilyosonline@gmail.com>
2019-11-14 15:03:58 +0500

    1

--- udoc-user/assets/js/Site.js
+++ udoc-user/assets/js/Site.js
@@ -441,6 +441,62 @@ $(document).ready(function(e) {
         dropdownCssClass: "custom-select2"
     });
 
-    // $('#datatable-dogovar tbody tr').webuiPopover({animation:'pop',trigger:'click', content:"<table class='info-table table-striped table'> <tbody> <tr> <td>Доверенность</td> <td>5</td> </tr> <tr> <td>Счёт на оплату</td> <td>7</td> </tr> <tr> <td>Счёт фактура</td> <td>14</td> </tr> <tr> <td>АКТ</td> <td>14</td> </tr> <tr> <td>ТТН</td> <td>5</td> </tr> <tr> <td>Срок окончания</td> <td>30.10.2019</td> </tr> </tbody></table>"});
 
+    $(function() {
+        var countFiles = 1,
+            $body = $('body'),
+            typeFileArea = ['txt', 'doc', 'docx', 'ods'],
+            coutnTypeFiles = typeFileArea.length;
+
+        //create new element
+        $body.on('click', '.files-wr label', function() {
+            var wrapFiles = $('.files-wr'),
+                newFileInput;
+
+            countFiles = wrapFiles.data('count-files') + 1;
+            wrapFiles.data('count-files', countFiles);
+
+            newFileInput = '<div class="one-file"><label for="file-' + countFiles + '">Прикрепить файл</label>' +
+                '<input type="file" name="file-' + countFiles + '" id="file-' + countFiles + '"><div class="file-item hide-btn">' +
+                '<span class="file-name"></span><span class="btn-del-file"><i class="icon wb-close"></i></span></div></div>';
+            wrapFiles.prepend(newFileInput);
+        });
+
+        //show text file and check type file
+        $body.on('change', 'input[type="file"]', function() {
+            var $this = $(this),
+                valText = $this.val(),
+                fileName = valText.split(/(\\|\/)/g).pop(),
+                fileItem = $this.siblings('.file-item'),
+                beginSlice = fileName.lastIndexOf('.') + 1,
+                typeFile = fileName.slice(beginSlice);
+
+            fileItem.find('.file-name').text(fileName);
+            if (valText != '') {
+                fileItem.removeClass('hide-btn');
+
+                for (var i = 0; i < coutnTypeFiles; i++) {
+
+                    if (typeFile == typeFileArea[i]) {
+                        $this.parent().addClass('has-mach');
+                    }
+                }
+            } else {
+                fileItem.addClass('hide-btn');
+            }
+
+            if (!$this.parent().hasClass('has-mach')) {
+                $this.parent().addClass('error');
+            }
+        });
+
+        //remove file
+        $body.on('click', '.btn-del-file', function() {
+            var elem = $(this).closest('.one-file');
+            elem.fadeOut(400);
+            setTimeout(function() {
+                elem.remove();
+            }, 400);
+        });
+    });
 });

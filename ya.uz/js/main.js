a1ab487a93e04885f6621e5d7be4dd32feda6d91
Ilyos Takhirov <ilyostakhirov@users.noreply.github.com>
2020-02-15 22:36:40 +0500

    1

--- ya.uz/js/main.js
+++ ya.uz/js/main.js
@@ -12,13 +12,4 @@ $(document).ready(function () {
         animateOut: 'fadeOut'
     })
 
-
-    $(".popular_news").niceScroll({
-        cursorwidth:4,
-        cursoropacitymin:0.4,
-        cursorcolor:'#2e4780',
-        cursorborder:'none',
-        cursorborderradius:5,
-        background:"rgba(20,20,20,0.3)",
-    });
 })
